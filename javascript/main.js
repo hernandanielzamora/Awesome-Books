660767bfe8a3ff51bc6532c955a43caa99f6f28e
Hernán Zamora <105683031+hernandanielzamora@users.noreply.github.com>
2023-03-22 13:49:47 -0300

    Solved issues
    The remove button is now fixed

--- javascript/main.js
+++ javascript/main.js
@@ -25,7 +25,7 @@ class UItask {
     const bookList = document.getElementById('book-list');
     const bookContent = document.createElement('div');
     bookContent.className = 'book';
-    bookContent.innerHTML = `<p class="book-title">"${book.title}"</p>
+    bookContent.innerHTML = `<p class="book-title">${book.title}</p>
                       <p class="book-author">by ${book.author}</p>
                       <button class="delete">Remove</button>`;
     bookList.appendChild(bookContent);
